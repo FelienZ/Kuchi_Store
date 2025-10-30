8119c510ab4e9b5e95b940d8b067736d2f3196d1
FelienZ <rajabfadh@gmail.com>
2025-10-30 22:05:12 +0700

    Improve Order Features: Prevent Insert New Row for Same Product that reordered, instead update order quantity

--- src/backend/services/orderServices.cjs
+++ src/backend/services/orderServices.cjs
@@ -11,20 +11,38 @@ async function verifyOrderItems(orderData) {
     if(product.price * qty !== price) throw new Error('Order Tidak Valid')
     return product;
 }
-
+async function checkMatchOrder(userId, orderData) {
+    const {id} = orderData
+    const {data} = await supabase.from('orders').select('id, qty').eq('product_id', id).eq('user_id', userId)
+    return data[0]
+}
 async function postOrder(userId, orderData) {
     const product = await verifyOrderItems(orderData)
+    const matchOrder = await checkMatchOrder(userId, orderData)
     const {id, price, qty} = orderData
+    if(matchOrder){
+        //update orderQty -> update Stock
+        const newQty = matchOrder.qty + qty
+        console.log('cek qty baru: ', newQty)
+        const newPrice = newQty * price
+        const {error: qtyError} = await supabase.from('orders').update({qty: newQty, total_price: newPrice}).eq('id', matchOrder.id)
+        const newStock = product.stock - qty
+        const {error: stockError} = await supabase.from('products').update({stock: newStock}).eq('id', id)
+        if(stockError) throw new Error(`[OrderServices::post, updatestock]: ${error}`)
+        if(qtyError) throw new Error(`[OrderServices::post, updateqty]: ${error}`)
+        return
+    }
     const {data, error} = await supabase.from('orders').insert([{user_id: userId, product_id: id, qty: qty, total_price: price}]).select('*')
     if(error) throw new Error(`[OrderServices::post]: ${error}`)
     if(data){
+        //update Stock
         const newStock = product.stock - qty
         const {error: stockError} = await supabase.from('products').update({stock: newStock}).eq('id', id)
         if(stockError) throw new Error(`[OrderServices::post, updatestock]: ${error}`)
     }
 }
 async function getAllOrder(userId) {
-    const { data, error } = await supabase.from('orders').select('*, product: products(id, shortname, url, price, type)').eq('user_id', userId)
+    const { data, error } = await supabase.from('orders').select('*, product: products(*)').eq('user_id', userId)
     if(error) throw new Error(`[OrderServices::get]: ${error}`)
     return data
 }
--- src/frontend/Checkout/CheckoutPage.jsx
+++ src/frontend/Checkout/CheckoutPage.jsx
@@ -1,4 +1,4 @@
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faMinus, faPlus, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import useFetchOrder from "../../hooks/fetchOrder";
@@ -6,7 +6,6 @@ import useFetchOrder from "../../hooks/fetchOrder";
 export default function CheckoutPage(){
   const [isLoading, setIsLoading] = useState(false)
   const [order] = useFetchOrder({setIsLoading})
-  console.log('cek order:', order)
   return(
     isLoading ? (<section className="min-h-screen grid place-content-center">
                 <span className="loading loading-bars loading-xl text-lime-500"></span>
@@ -15,29 +14,37 @@ export default function CheckoutPage(){
                 <p className="font-bold text-2xl flex items-center gap-3 pb-2 text-neutral"> <FontAwesomeIcon icon={faShoppingCart}/> Your Shopping Cart</p>
                 <div className="grid gap-5">
                   {order.map(i=>(
-                    <div key={i.id} className="flex bg-white drop-shadow-sm items-center rounded-md p-4 justify-between">
-                      <div className="p-3 border border-gray-300 rounded-md">
-                        <img src={i.product.url} className="size-15"/>
+                    <div key={i.id} className="flex max-lg:grid min-sm:grid-cols-[auto_auto] border place-items-center gap-3 w-full bg-white drop-shadow-sm items-center rounded-md p-4">
+                      <div className="p-3 border border-gray-300 rounded-md max-sm:place-self-center">
+                        <img src={i.product.url} className="size-15 max-lg:size-25"/>
                       </div>
-                      <div className="name flex flex-col items-center gap-3">
+                      <div className="detail flex items-center justify-around w-full max-[400px]:text-xs max-lg:flex-col max-lg:gap-4 max-lg:p-5">
+                        <div className="name flex lg:flex-col items-center gap-3 max-lg:w-full justify-center">
                         <p className="font-bold text-neutral">{i.product.shortname??i.product.name}</p>
-                        <p>{i.product.type}</p>
+                        <p className="max-lg:hidden">{i.product.type}</p>
                       </div>
-                      <div className="name flex flex-col items-center gap-3">
-                        <p>Harga</p>
+                      <div className="name flex lg:flex-col items-center gap-3 max-lg:w-full justify-center">
+                        <p>Status Pemesanan:</p>
+                        <p className={`badge badge-outline ${i.status.trim()==='done' ? 'badge-success' : 'badge-error'}`}>{i.status}</p>
+                      </div>
+                      <div className="name flex lg:flex-col items-center gap-3 max-lg:w-full justify-center">
+                        <p>Harga:</p>
                         <p className="font-bold text-lime-500">{i.product.price.toLocaleString('id-ID',{style:'currency', currency: 'IDR'})}</p>
                       </div>
-                      <div className="name flex flex-col items-center gap-3">
-                        <p>Jumlah Pemesanan</p>
+                      <div className="name flex lg:flex-col items-center gap-3 max-lg:w-full justify-center">
+                        <p>Jumlah Pemesanan:</p>
                         <div className="flex items-center gap-2">
+                          <button className="btn btn-neutral size-8"><FontAwesomeIcon icon={faMinus}/></button>
                           <p className="font-bold text-lime-500">{i.qty}</p>
+                          <button className="btn btn-neutral size-8"><FontAwesomeIcon icon={faPlus}/></button>
                         </div>
                       </div>
-                      <div className="name flex flex-col items-center gap-3">
-                        <p>Total Harga</p>
+                      <div className="name flex lg:flex-col items-center gap-3 max-lg:w-full justify-center">
+                        <p>Total Harga:</p>
                         <p className="font-bold text-lime-500">{i.total_price.toLocaleString('id-ID',{style:'currency', currency: 'IDR'})}</p>
                       </div>
-                      <button className="btn btn-neutral btn-outline rounded-full">Cancel</button>
+                      <button className="btn btn-neutral btn-outline rounded-full max-lg:w-full">Cancel</button>
+                      </div>
                     </div>
                   ))}
                 </div>
